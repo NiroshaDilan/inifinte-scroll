c274ffa7bafa3799e687e7b4fab3fe6754d03d6d
NiroshaDilan <dilan.vista@gmail.com>
2019-10-31 00:39:47 +0530

    owl datetime picker with utc conversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'infinite-scroll';
+export class AppComponent implements OnInit{
+
+  angForm: FormGroup;
+  startAt = new Date();
+  myEmail: string = 'myemail@gmail.com';
+
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.angForm = this.fb.group({
+      insertDate: [''],
+      email: [''],
+      password: ['']
+    });
+  }
+
+  onClickSubmit(email, password) {
+
+
+    this.angForm.get('insertDate').setValue(this.startAt);
+    console.log(email + " " + password);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {OwlDateTimeModule, OwlNativeDateTimeModule} from 'ng-pick-datetime';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ReacttiveFormComponent} from './reacttive-form/reacttive-form.component';
+import {HttpClientModule} from '@angular/common/http';
+import {UserComponent} from './user/user.component';
+import {OwlMomentDateTimeModule} from 'ng-pick-datetime/date-time/adapter/moment-adapter/moment-date-time.module';
+import {OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS} from 'ng-pick-datetime/date-time/adapter/moment-adapter/moment-date-time-adapter.class';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReacttiveFormComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    OwlMomentDateTimeModule
   ],
-  providers: [],
+  providers: [{provide: OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS, useValue: {useUtc: false}}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
